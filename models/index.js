9ceb40aaceb060afe8a6f027e78d80d5547e64a9
Mathew Poppell <mpoppell1002@gmail.com>
2023-11-04 12:10:17 -0400

    initial commit, set up folder structures

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const User = require('./User');
+const Entry = require('./Entry');
+const Comment = require('./Comment');
+
+User.hasMany(Entry, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Entry.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Entry.hasMany(Comment,{
+  foreignKey: 'entry_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+module.exports = { User, Entry, Comment };
